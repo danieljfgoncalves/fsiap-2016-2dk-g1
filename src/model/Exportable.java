12a16d48e15e612a38f53caa1020b0a95bde0148
Tiago Correia <1151031@isep.ipp.pt>
2016-11-19 00:58:37 +0000

    Implementação da interface Exportable [implementação][model]

---
+++ src/model/Exportable.java
@@ -0,0 +1,21 @@
+/*
+ * Package location for Model concepts.
+ */
+package model;
+
+/**
+ * Interface for file exportation.
+ *
+ * @author Eric Amaral 1141570
+ * @author Daniel Gonçalves 1151452
+ * @author Ivo Ferro 1151159
+ * @author Tiago Correia 1151031
+ */
+public interface Exportable {
+    
+    /**
+     * Exports data.
+     */
+    public abstract void export();
+    
+}
