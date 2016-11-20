51bc15e1e5db1fea7d197286244b556a95a32448
ThisTruckThor <ejsfae@gmail.com>
2016-11-20 18:06:27 +0000

    Refactors  class ShowCuriositiesController [refactoring][controller][issue #12]

--- src/controller/ShowCuriositiesController.java
+++ src/controller/ShowCuriositiesController.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Package location for Model concepts.
  */
 package controller;
 
@@ -10,8 +8,12 @@ import model.Curiosity;
 import model.Simulator;
 
 /**
+ * The controller to show curiosities.
  *
- * @author Eric Jorge Schipper Ferreira do Amaral 1141570@isep.ipp.pt TurmaDN
+ * @author Daniel Gonçalves 1151452
+ * @author Eric Amaral 1141570
+ * @author Ivo Ferro 1151159
+ * @author Tiago Correia 1151031
  */
 public class ShowCuriositiesController {
 
