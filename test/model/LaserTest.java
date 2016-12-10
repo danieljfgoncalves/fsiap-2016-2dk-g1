c1b6e096e3069b125c78ae9d3833fe6ebe5e2459
ivoferroo <1151159@isep.ipp.pt>
2016-12-10 21:32:57 +0000

    implements material tests [implementation]

--- test/model/LaserTest.java
+++ test/model/LaserTest.java
@@ -3,8 +3,6 @@
  */
 package model;
 
-import model.calculations.Calculable;
-import model.calculations.MeltingCalculus;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -18,12 +16,12 @@ import static org.junit.Assert.*;
  * @author Tiago Correia 1151031
  */
 public class LaserTest {
-    
+
     /**
      * Instance to be tested
      */
     private Laser instance;
-    
+
     @Before
     public void setUp() {
         instance = new Laser(10600e-9, new Gas("CO2", 9200E-9, 11400E-9), 2e-3,
@@ -36,10 +34,10 @@ public class LaserTest {
     @Test
     public void testGetWavelength() {
         System.out.println("getWavelength");
-        
+
         Double expResult = 10600e-9;
         Double result = instance.getWavelength();
-        
+
         assertEquals(expResult, result, 0.01d);
     }
 
@@ -49,10 +47,10 @@ public class LaserTest {
     @Test
     public void testSetWavelength() {
         System.out.println("setWavelength");
-        
+
         Double wavelength = 10000e-9;
         instance.setWavelength(wavelength);
-        
+
         assertEquals(instance.getWavelength(), wavelength, 0.01d);
     }
 
@@ -62,10 +60,10 @@ public class LaserTest {
     @Test
     public void testGetGas() {
         System.out.println("getGas");
-        
+
         Gas expResult = new Gas("CO2", 9200E-9, 11400E-9);
         Gas result = instance.getGas();
-        
+
         assertEquals(expResult, result);
     }
 
@@ -75,10 +73,10 @@ public class LaserTest {
     @Test
     public void testSetGas() {
         System.out.println("setGas");
-        
+
         Gas gas = new Gas("abc", 1d, 10d);
         instance.setGas(gas);
-        
+
         assertEquals(instance.getGas(), gas);
     }
 
@@ -88,10 +86,10 @@ public class LaserTest {
     @Test
     public void testGetMaterial() {
         System.out.println("getMaterial");
-        
+
         Material expResult = new Material("Gold", 63000.0, 130.0, 19320.0, 2800.0, 1063.0, true);
         Material result = instance.getMaterial();
-        
+
         assertEquals(expResult, result);
     }
 
@@ -101,10 +99,10 @@ public class LaserTest {
     @Test
     public void testSetMaterial() {
         System.out.println("setMaterial");
-        
+
         Material material = new Material();
         instance.setMaterial(material);
-        
+
         assertEquals(instance.getMaterial(), material);
     }
 
@@ -114,10 +112,10 @@ public class LaserTest {
     @Test
     public void testGetMaterialThickness() {
         System.out.println("getMaterialThickness");
-        
+
         Double expResult = 4e-3;
         Double result = instance.getMaterialThickness();
-        
+
         assertEquals(expResult, result, 0.01d);
     }
 
@@ -127,10 +125,10 @@ public class LaserTest {
     @Test
     public void testSetMaterialThickness() {
         System.out.println("setMaterialThickness");
-        
+
         Double materialThickness = 2e-3;
         instance.setMaterialThickness(materialThickness);
-        
+
         assertEquals(instance.getMaterialThickness(), materialThickness, 0.01d);
     }
 
@@ -140,10 +138,10 @@ public class LaserTest {
     @Test
     public void testGetBeamDiameter() {
         System.out.println("getBeamDiameter");
-        
+
         Double expResult = 2e-3;
         Double result = instance.getBeamDiameter();
-        
+
         assertEquals(expResult, result);
     }
 
@@ -153,10 +151,10 @@ public class LaserTest {
     @Test
     public void testSetBeamDiameter() {
         System.out.println("setBeamDiameter");
-        
+
         Double beamDiameter = 6e-3;
         instance.setBeamDiameter(beamDiameter);
-        
+
         assertEquals(instance.getBeamDiameter(), beamDiameter, 0.01d);
     }
 
@@ -166,10 +164,10 @@ public class LaserTest {
     @Test
     public void testGetFocalPointArea() {
         System.out.println("getFocalPointArea");
-        
+
         Double expResult = 3.14e-6d;
         Double result = instance.getFocalPointArea();
-        
+
         assertEquals(expResult, result, 0.01);
     }
 
@@ -179,10 +177,10 @@ public class LaserTest {
     @Test
     public void testSetFocalPointArea() {
         System.out.println("setFocalPointArea");
-        
+
         Double focalPointArea = 4.25e-6d;
         instance.setFocalPointArea(focalPointArea);
-        
+
         assertEquals(instance.getFocalPointArea(), focalPointArea, 0.01d);
     }
 
@@ -192,10 +190,10 @@ public class LaserTest {
     @Test
     public void testGetMaxPower() {
         System.out.println("getMaxPower");
-        
+
         Double expResult = 80d;
         Double result = instance.getMaxPower();
-        
+
         assertEquals(expResult, result, 0.01d);
     }
 
@@ -205,10 +203,10 @@ public class LaserTest {
     @Test
     public void testGetFactor() {
         System.out.println("getFactor");
-        
+
         Float expResult = 1f;
         Float result = instance.getFactor();
-        
+
         assertEquals(expResult, result);
     }
 
@@ -218,10 +216,10 @@ public class LaserTest {
     @Test
     public void testSetFactor() {
         System.out.println("setFactor");
-        
+
         Float factor = 3f;
         instance.setFactor(factor);
-        
+
         assertEquals(instance.getFactor(), factor, 0.01);
     }
 
@@ -231,16 +229,16 @@ public class LaserTest {
     @Test
     public void testEquals() {
         System.out.println("equals");
-        
+
         Object obj = null;
         assertFalse(instance.equals(obj));
-        
+
         obj = new Laser(1d, new Gas("a", 1d, 1d), 1d, new Material(), 1d);
         assertFalse(instance.equals(obj));
-        
+
         obj = new Laser(10600e-9, new Gas("CO2", 9200E-9, 11400E-9), 2e-3,
                 new Material("Gold", 63000.0, 130.0, 19320.0, 2800.0, 1063.0, true), 4e-3);
         assertTrue(instance.equals(obj));
     }
-    
+
 }
